b2d09057f6ced2e041d2fa1e19ac78665556df65
Nabil A <nazam9519@gmail.com>
2026-03-04 01:24:20 -0500

    fixed gitignore and created a base transferrable set

---
+++ src/.config/settings.el
@@ -0,0 +1,18 @@
+;(require 'use-package)
+(defun setup-backups ()
+  "Configure Emacs backups."
+  (setq backup-directory-alist '(("." . "~/.emacs_backups"))
+        backup-by-copying t
+        delete-old-versions t
+        kept-new-versions 3
+        kept-old-versions 1
+        version-control t))
+
+(setq emacs-config-dir user-emacs-directory)
+(add-to-list 'load-path
+             (expand-file-name "modules" emacs-config-dir))
+
+(setup-backups)
+;; (setq backup-directory-alist
+;;       `(("." . "~/.emacs_backups")))
+(provide 'settings)
---
+++ src/modules/evilvim.el
@@ -0,0 +1,36 @@
+(require 'use-package)
+(defun release-the-evil ()
+  "toggle on and off of evil(vi mode)"
+  (interactive)
+  (use-package evil
+  :init
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (if evil-mode
+    (progn
+	(message "turning evil mode off")
+	(evil-mode 0))
+    (progn
+      (message "turning on evil mode")
+      (evil-mode 1)))))
+;;
+  
+(defun toggle-line-numbers ()
+  "Toggle between absolute and relative line numbers."
+  (interactive)
+  (setq display-line-numbers-type
+        (if (eq display-line-numbers-type 'relative)
+            t
+          'relative))
+  (global-display-line-numbers-mode 0)
+  (global-display-line-numbers-mode 1))
+
+;;code starts					
+;(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode 1)
+(release-the-evil)
+
+
+
+(provide 'evilvim)
